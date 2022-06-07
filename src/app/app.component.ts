a91ff8d7fa1d611b14a630a9f2d4f632dd7c5965
Hamed Sedighi <hamedsedighi@gmail.com>
2022-06-07 16:59:01 +0200

    setup base app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'grzybogranie-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'grzybogranie';
-}
+export class AppComponent {}
