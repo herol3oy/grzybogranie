d14998ecb0ac63e987669e697d3ca85b6d3423e6
Hamed Sedighi <hamedsedighi@gmail.com>
2022-06-07 16:26:30 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'grzybogranie-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'grzybogranie';
+}
